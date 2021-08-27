0053b1b4b20a1ca34dfc59b4e4a0f3f5bc1675de
julio cesar <81329552+juliout@users.noreply.github.com>
2021-08-27 12:20:32 -0300

    primeiro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Inputs from "./components/inputs";
+import './app.css'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pai">      
+        <form className="form">
+          <Inputs type="text" className="input is-primary is-medium Mlargura" name="nome" id="iNome">
+            Nome Completo:
+          </Inputs>
+          <Inputs type="email" className="input is-primary is-medium Mlargura" name="email" id="iemail"> 
+            Email:
+          </Inputs>
+          <Inputs type="password" className="input is-primary is-medium Plargura" name="senha" id="isenha">
+            Senha:
+          </Inputs>
+          <Inputs  type="password" className="input is-primary is-medium Plargura" name="resenha" id="iresenha">
+            Re-senha:
+          </Inputs>
+          <div className="buttons">
+            <button className="button is-primary is-large">Enviar</button>
+            <button className="button is-danger">Limpar</button>
+          </div>
+
+        </form>      
     </div>
   );
-}
-
-export default App;
+  }
